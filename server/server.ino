6756c345eecd395664d94b71b4377e593c2a3c78
Kevin Casey <kevin@kevincasey.com>
2020-07-07 21:32:18 -0400

    cleaned up comments and formatting

--- server/server.ino
+++ server/server.ino
@@ -12,7 +12,7 @@ ESP8266WebServer server(80);
 void handleRoot();
 void handleNotFound();
 
-unsigned long dbgTime; 
+unsigned long loopTime = 0; 
 bool printLoopTime = false;
 
 void setup()
@@ -29,30 +29,34 @@ void setup()
   Serial.println(WiFi.softAPIP());
 
   // start the webserver
-  server.on("/", handleRoot);               // Call the 'handleRoot' function when a client requests URI "/"
-  server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
-  server.begin();                           // Actually start the server
+  server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
+  server.begin();
   Serial.println("HTTP server started");
-}
+} // end setup()
 
 void loop()
 {
-  dbgTime = millis();
-  server.handleClient();                    // Listen for HTTP requests from clients
-//  Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
-//  delay(3000);
+  loopTime = millis();
+  
+  server.handleClient();
+  
   if (printLoopTime) {
-    Serial.printf("total loop time was %d\n", millis() - dbgTime);
+    Serial.printf("total loop time was %dms\n", millis() - loopTime);
     printLoopTime = false;
   }
-}
+} // end loop()
 
+
+// Send HTTP status 200 (Ok) and send some text to the browser/client
 void handleRoot() {
-  server.send(200, "text/plain", "Hello world!");   // Send HTTP status 200 (Ok) and send some text to the browser/client
+  server.send(200, "text/plain", "Hello world!");
   printLoopTime = true;
 }
 
+
+// Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
 void handleNotFound() {
-  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+  server.send(404, "text/plain", "404: Not found");
   printLoopTime = true;
 }
